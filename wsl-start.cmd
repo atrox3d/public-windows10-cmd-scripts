2fc6f16fa834b4948700a3382d63e73be9244026
atrox3d <atrox3d@gmail.com>
2021-06-19 06:56:13 +0200

    better log macros

--- wsl-start.cmd
+++ wsl-start.cmd
@@ -4,15 +4,17 @@ echo ###########################################################################
 echo # wsl-start.cmd^| START
 echo ###############################################################################
 
-set INFO=[ INFO]
-set OK=[   OK]
-set ERROR=[ERROR]
+set INFO=echo [ INFO]
+set OK=echo [   OK]
+set ERROR=echo [ERROR]
+set DEBUG=REM echo [DEBUG]
 
-echo %INFO% shutdown docker desktop...
+%INFO% shutdown docker desktop...
 wmic process where "name like '%%docker desktop%%'" get processid, name, commandline
 taskkill /im  "docker desktop.exe" /f
 
-echo %INFO% shutdown wsl...
+%INFO% shutdown wsl...
+%DEBUG% wsl --shutdown
 wsl --shutdown
 
 set IPADDRESS=192.168.1.10
@@ -20,54 +22,54 @@ set UNCPATH=\\%IPADDRESS%\c$\users\***REMOVED***\code
 set UNCUSER=***REMOVED***
 set UNCPASSWORD=***REMOVED***
 
-echo %INFO% check for host %IPADDRESS%...
+%INFO% check for host %IPADDRESS%...
 ping %IPADDRESS% | find "TTL" >nul
 if not errorlevel 1 (
 
-	echo %OK% host %IPADDRESS% is online
+	%OK% host %IPADDRESS% is online
 
-	echo %INFO% check for %UNCPATH%...
+	%INFO% check for %UNCPATH%...
 	if exist %UNCPATH% (
 	
-		echo %OK% %UNCPATH% exists
+		%OK% %UNCPATH% exists
 
-		echo %INFO% umount %UNCPATH%...
+		%INFO% umount %UNCPATH%...
 		net use %UNCPATH% /d /y
 
-		echo %INFO% mount %UNCPATH%...
+		%INFO% mount %UNCPATH%...
 		net use %UNCPATH% /u:%UNCUSER% %UNCPASSWORD%
 
-		echo %INFO% umount Z:...
+		%INFO% umount Z:...
 		net use Z: /d /y
 
-		echo %INFO% mount z: %UNCPATH%
+		%INFO% mount z: %UNCPATH%
 		net use z: %UNCPATH% /u:%UNCUSER% %UNCPASSWORD%
 
 		echo ###############################################################################
 		net use
 		echo ###############################################################################
 
-		echo %INFO% mount wsl ubuntu 18 fstab...
+		%INFO% mount wsl ubuntu 18 fstab...
 		wsl -d ubuntu18 -u root mount -a
 
-		echo %INFO% mount wsl ubuntu 20 fstab...
+		%INFO% mount wsl ubuntu 20 fstab...
 		wsl -d ubuntu20 -u root mount -a
 		
 	) else (
-		echo %ERROR% %UNCPATH% does NOT exist
-		echo %ERROR% cannot mount network shares
+		%ERROR% %UNCPATH% does NOT exist
+		%ERROR% cannot mount network shares
 	)
 ) else (
-	echo %ERROR% address %IPADDRESS% is not available
+	%ERROR% address %IPADDRESS% is not available
 )
 
-echo %INFO%  start wsl ubuntu 18 and ssh...
+%INFO%  start wsl ubuntu 18 and ssh...
 wsl -d ubuntu18 -u root service ssh restart
 
-echo %INFO% start wsl ubuntu 20 and ssh...
+%INFO% start wsl ubuntu 20 and ssh...
 wsl -d ubuntu20 -u root service ssh restart
 
-echo %INFO% start docker desktop
+%INFO% start docker desktop
 REM start /b "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
